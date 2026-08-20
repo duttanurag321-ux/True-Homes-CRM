8c6a80fa9a77b1439c1266d94506bcbbefdb6a1e
duttanurag321-ux <duttanurag321@gmail.com>
2026-08-20 13:25:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/true-homes-crm">
+    <BrowserRouter basename="/True-Homes-CRM">
       <AuthProvider>
         <App />
       </AuthProvider>
